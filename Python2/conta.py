accc6b247e44e9064dd50922cbd31a5b050a185f
Ana Manuella <ana.manuellar@gmail.com>
2022-08-21 09:04:05 -0300

    executando o prog

---
+++ Python2/conta.py
@@ -0,0 +1,6 @@
+class Contas:
+    def __init__(self, titular, conta, saldo, vip):
+        self.titular = titular
+        self.conta = conta
+        self.saldo = saldo
+        self.vip = vip
---
+++ Python2/prog.py
@@ -0,0 +1,5 @@
+from conta import Contas
+
+contaBradesco = Contas("Ana", "1234-5", 12, False)
+print(contaBradesco.titular)
+print(contaBradesco.saldo)
