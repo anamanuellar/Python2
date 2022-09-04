02c344603b7312c9c5f210d15a38914adc81f67e
Ana Manuella <ana.manuellar@gmail.com>
2022-09-04 10:09:22 -0300

    atividade

--- Python2/Atividade 2/composicao.py
+++ Python2/Atividade 2/composicao.py
@@ -1,4 +1,7 @@
 #Associacao entre classes:
+from xmlrpc.client import boolean
+
+
 class Salarios:
     def __init__(self, qtHoras, valorHora, adicional=0):
         self.qtHoras = qtHoras
@@ -10,11 +13,25 @@ class Salarios:
     
 #Classe Todo:
 class Funcionarios:
-    def __init__(self, nome, cpf, endereço, qtHoras, valorHora, adicional):
+    def __init__(self, nome, cpf, endereco, qtHoras, valorHora, adicional, nomeDep=0, qtFuncionarios=0):
         self.nome = nome
         self.cpf = cpf
-        self.endereço= endereço
+        self.endereço= endereco
         self.salario = Salarios(qtHoras, valorHora, adicional)
+        self.departamento = Departamentos(nomeDep, qtFuncionarios)
+   
+    def imprimirDep(self):
+        return(self.departamento)
         
-vendedor = Funcionarios('Marta Silva', '123.456.678-90', 'Ruas das Flores, 007', 15, 100, 0.1)
-print(vendedor.nome, ' = R$', vendedor.salario.calcularSalarios())
+class Departamentos:
+    def __init__(self, nomeDep, qtFuncionarios, adm=False):
+        self.nomeDep = nomeDep
+        self.qtFuncionarios = qtFuncionarios
+        self.adm = adm
+        
+ 
+        
+vendedor = Funcionarios('Marta Silva', '123.456.678-90', 'Ruas das Flores, 007', 15, 100, 0.1, 'Compras', 32)
+print(vendedor.nome, ' = R$', vendedor.salario.calcularSalarios(), 'Departamento: ', vendedor.imprimirDep)
+funcionario = Departamentos('Compras', 32)
+print('Departamento: ', funcionario.nomeDep, ' Quantidade de funcionarios: ', funcionario.qtFuncionarios)
